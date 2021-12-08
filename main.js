783da7d92a736909ef55393e09201c743d1710dd
ivan ois <ivoxelkapo@gmail.com>
2021-12-08 16:45:00 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+var n1;
+var n2;
+var n3;
+var sumade3;
+n1 = parseInt(prompt('ingresar numero 1'))
+n2 = parseInt(prompt('ingresar numero 2'))
+n3 = parseInt(prompt('ingresar numero 3'))
+sumade3 = n1 + n2 + n3;
+alert ('la suma es' + " " + sumade3)
